54c4c995e47cb48fcbed1bbf7ee96ea14dc5084f
JsToys <94050820+Hipgineer@users.noreply.github.com>
2021-11-23 23:21:30 +0900

    minor
    minor

--- main.jl
+++ main.jl
@@ -15,11 +15,12 @@ SIMULDATA   = JuliaPBF.Solver.PreProcessing(ANALYDATA) # FillInitialBox, GenInit
 curr_time = 0.0 
 ii        = 0 
 while curr_time < ANALYDATA.endTime
-# for ii in 1:100 #Int(floot(ANALYDATA.EndTimeStep))
     curr_time += ANALYDATA.timeStep.dt
-    @printf("Time = %10.5f  spends ",curr_time)
+    @printf("Time = %10.5f \n",curr_time)
+    @printf("  Update for          :")
     @time JuliaPBF.Solver.Update(SIMULDATA, ANALYDATA);
     
     ii += 1
-    JuliaPBF.IO_dev.Writing_ascii_file(SIMULDATA, "out__"*lpad(ii,5,"0"))
+    @printf("  Writing Output for  :")
+    @time JuliaPBF.IO_dev.Writing_ascii_file(SIMULDATA, "out__"*lpad(ii,5,"0"))
 end 
